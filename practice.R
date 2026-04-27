454e29acf2daa3ae3bef174da8430accc8aeeaec
金谷大也 <daiyamond.king@gmail.com>
2026-04-27 13:24:56 +0900

    practice.Rを追加

---
+++ practice.R
@@ -0,0 +1,17 @@
+a <- 0
+b <- 0
+result.even <- c()
+result.odd <- c()
+
+for (i in 1:100) {
+  if (i %% 2 == 0) {
+    a <- a + i
+    result.even <- c(result.even, a)
+  } else {
+    b <- b + i
+    result.odd <- c(result.odd, b)
+  }
+}
+
+print(result.even)
+print(result.odd)
